51912509e5e78028c9d0f374212eb8978148fffa
Prakash <prakashsochandrasekar@gmail.com>
2021-08-20 10:58:48 +0530

    First commit

---
+++ upload/src/DownloadServlet.java
@@ -0,0 +1,48 @@
+
+import java.io.*;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DownloadServlet
+ */
+@WebServlet(name = "DownloadServlet1", urlPatterns = { "/DownloadServlet1" })
+public class DownloadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DownloadServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");  
+		PrintWriter out = response.getWriter();  
+		String filename = "abc.txt";   
+		String filepath = "e:\\";   
+		response.setContentType("APPLICATION/OCTET-STREAM");   
+		response.setHeader("Content-Disposition","attachment; filename=\"" + filename + "\"");   
+		  
+		FileInputStream fileInputStream = new FileInputStream(filepath + filename);  
+		            
+		int i;   
+		while ((i=fileInputStream.read()) != -1) {  
+		out.write(i);   
+		}   
+		fileInputStream.close();   
+		out.close();   
+		}  
+	}
+
+
